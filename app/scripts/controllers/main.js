2b394bd03e054a9ec445f392d706fb36a72f3193
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-11-09 06:50:35 -0600

    indenting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,9 +7,9 @@
  * # MainCtrl
  * Controller of the devFlowApp
  */
-var myapp = angular.module('devFlowApp');
+ var myapp = angular.module('devFlowApp');
 
-myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http, md5) {
+ myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http, md5) {
     $scope.results = [];
     $scope.hashedEmail = "http://www.gravatar.com/avatar/";
     
@@ -18,40 +18,40 @@ myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http, m
         var sync = $firebase(ref);
         $http.get('https://devflow.firebaseio.com/users.json')
         .success(function(data) {
-        var hitCount = 0;
-        $scope.results = [];
-        if ($scope.query)
-        {
-            var magic = new RegExp(".*"+($scope.query.toLowerCase())+".*");
-            for (var key in data) {
-                var obj = data[key];
-                if (magic.test( (obj.name.toLowerCase()) )) {
-                    ++hitCount;
-                    obj.hashedEmail = $scope.hashedEmail + md5.createHash(obj.email);
-                    $scope.results.push(obj);
+            var hitCount = 0;
+            $scope.results = [];
+            if ($scope.query)
+            {
+                var magic = new RegExp(".*"+($scope.query.toLowerCase())+".*");
+                for (var key in data) {
+                    var obj = data[key];
+                    if (magic.test( (obj.name.toLowerCase()) )) {
+                        ++hitCount;
+                        obj.hashedEmail = $scope.hashedEmail + md5.createHash(obj.email);
+                        $scope.results.push(obj);
+                    }
+                }
+                if (hitCount > 0) {
+                    $scope.name="Found " + hitCount + " Users with that name";
+                } else {
+                    $scope.name="Found no users by that name";
+                }
+            } else {
+                var magic = new RegExp(".*");
+                for (var key in data) {
+                    var obj = data[key];
+                    if (magic.test( (obj.name.toLowerCase()) )) {
+                        ++hitCount;
+                        obj.hashedEmail = $scope.hashedEmail + md5.createHash(obj.email);
+                        $scope.results.push(obj);
+                    }
+                }
+                if (hitCount > 0) {
+                    $scope.name="Found " + hitCount + " Users with that name";
+                } else {
+                    $scope.name="Found no users by that name";
                 }
             }
-            if (hitCount > 0) {
-                $scope.name="Found " + hitCount + " Users with that name";
-            } else {
-                $scope.name="Found no users by that name";
-            }
-        } else {
-            var magic = new RegExp(".*");
-            for (var key in data) {
-                var obj = data[key];
-                if (magic.test( (obj.name.toLowerCase()) )) {
-                    ++hitCount;
-                    obj.hashedEmail = $scope.hashedEmail + md5.createHash(obj.email);
-                    $scope.results.push(obj);
-                }
-            }
-            if (hitCount > 0) {
-                $scope.name="Found " + hitCount + " Users with that name";
-            } else {
-                $scope.name="Found no users by that name";
-            }
-        }
-    });
-  };
+        });
+};
 });
