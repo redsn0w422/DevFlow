904ae133b3ff88ae7610a15a029befc2e7fa94a6
Niraj Pant <nirajpant7@gmail.com>
2014-11-09 00:19:40 -0600

    add gravatar image

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,11 +8,12 @@
  * Controller of the devFlowApp
  */
 angular.module('devFlowApp')
-  .controller('ProfileCtrl', function ($scope, $firebase, $routeParams, $http) {
+  .controller('ProfileCtrl', function ($scope, $firebase, $routeParams, $http, md5) {
     var ref = new Firebase('https://devflow.firebaseio.com/users');
 	var sync = $firebase(ref);
 	$scope.user = {};
 	$scope.foundUsername = false;
+	$scope.hashedEmail = "http://www.gravatar.com/avatar/";
 
 	$http.get('https://devflow.firebaseio.com/users.json')
 	.success(function(data) {
@@ -23,10 +24,9 @@ angular.module('devFlowApp')
 			if (obj.name === $routeParams.username) {
 				console.log('works');
 				$scope.user = obj;
+				console.log($scope.user);
 				$scope.foundUsername = true;
-			}
-			else {
-				console.log('no');
+				$scope.hashedEmail+=md5.createHash($scope.user.email);
 			}
 		}
 	});
