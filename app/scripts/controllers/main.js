d704171e10f67b90f5834413b3b4fd9af6af3874
icehockey38 <dillonhammond@gmail.com>
2014-11-08 23:37:23 -0600

    Implemented getting user list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,34 @@
  * # MainCtrl
  * Controller of the devFlowApp
  */
-angular.module('devFlowApp')
-  .controller('MainCtrl', function ($scope) {
+var myapp = angular.module('devFlowApp');
+/*
+myapp.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+  });
+*/
+myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http) {
+    var ref = new Firebase('https://devflow.firebaseio.com/users');
+	var sync = $firebase(ref);
+	$scope.user = {};
+	$scope.name = "Nirajpant7";
+	$http.get('https://devflow.firebaseio.com/users.json')
+	.success(function(data) {
+		console.log(data);
+
+		for (var key in data) {
+			var obj = data[key];
+			if (obj.name === $scope.name) {
+				console.log('works');
+				$scope.user = obj;
+			}
+			else {
+				console.log('no');
+			}
+		}
+	});
   });
