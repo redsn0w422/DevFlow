0ef3f08745e68eefc1881dca95ba3cdbd2ab4e2a
icehockey38 <dillonhammond@gmail.com>
2014-11-08 19:28:37 -0600

    Addwd files

--- devflow/devflow.js
+++ devflow/devflow.js
@@ -1,24 +1,53 @@
+comments = new Meteor.Collection("comments");
+
+Meteor.methods({
+ addFav: function (name, fav_name) {
+      var user = Meteor.users.findOne({'username': fav_name});
+      var favList = user.fav_list;
+      favList.push(name);
+      Meteor.users.update({'username': fav_name}, {$set: {'fav_list': favList}});
+  },
+  comments: function(comment_text, user_profile, user_property, time) {
+
+  }
+});
+
+Router.route('/', function () {
+  this.render('Main');
+});
+
+Router.route('/explore', function () {
+    this.render('Explore');
+});
+
+Router.route('/login', function () {
+  this.render('Login');
+});
+
+Router.route('/signup', function () {
+  this.render('Signup');
+});
+
+
+Router.route('/profile/:username', function () {
+  this.render('Profile', {
+    user: username
+  });
+});
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+  Meteor.subscribe('userData');
+  Meteor.subscribe('comments');
 }
 
 if (Meteor.isServer) {
   Accounts.onCreateUser(function(options, user){
     // define all of the variables for the user
+    return user;
   });
 
   Meteor.publish("userData", function() {
