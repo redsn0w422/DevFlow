aba2576f895ada671333d1a4298ff5b4efcf726d
icehockey38 <dillonhammond@gmail.com>
2014-11-09 01:03:36 -0600

    Stable before regex

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,21 +10,22 @@
 var myapp = angular.module('devFlowApp');
 
 myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http) {
+    $scope.results = [];
     $scope.searchOnClick = function () {
         var ref = new Firebase('https://devflow.firebaseio.com/users');
         var sync = $firebase(ref);
-        $scope.user = {};
         $http.get('https://devflow.firebaseio.com/users.json')
         .success(function(data) {
         var hitCount = 0;
-        console.log("succeeded at getting users");
+        $scope.results = [];
             for (var key in data) {
                 var obj = data[key];
                 if (obj.name === $scope.query) {
-                    $scope.user = obj;
                     ++hitCount;
+                    $scope.results.push(obj.name);
                 }
             }
+            console.log($scope.results);
             if (hitCount > 0) {
                 $scope.name="Found " + hitCount + " Users with that name";
             } else {
