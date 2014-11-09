6824662c2d1fb811923d57697361b0db79604e03
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-11-09 06:23:07 -0600

    styling of results page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,8 +9,10 @@
  */
 var myapp = angular.module('devFlowApp');
 
-myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http) {
+myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http, md5) {
     $scope.results = [];
+    $scope.hashedEmail = "http://www.gravatar.com/avatar/";
+    
     $scope.searchOnClick = function () {
         var ref = new Firebase('https://devflow.firebaseio.com/users');
         var sync = $firebase(ref);
@@ -23,6 +25,7 @@ myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http) {
                 var obj = data[key];
                 if (magic.test( (obj.name.toLowerCase()) )) {
                     ++hitCount;
+                    obj.hashedEmail = $scope.hashedEmail + md5.createHash(obj.email);
                     $scope.results.push(obj);
                 }
             }
