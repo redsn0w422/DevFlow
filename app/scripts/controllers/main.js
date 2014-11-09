907895778226ab49ac99defb5dbbdb17bf9756a1
icehockey38 <dillonhammond@gmail.com>
2014-11-08 23:59:51 -0600

    Working user search in code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,33 +8,28 @@
  * Controller of the devFlowApp
  */
 var myapp = angular.module('devFlowApp');
-/*
-myapp.controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
-*/
-myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http) {
-    var ref = new Firebase('https://devflow.firebaseio.com/users');
-	var sync = $firebase(ref);
-	$scope.user = {};
-	$scope.name = "Nirajpant7";
-	$http.get('https://devflow.firebaseio.com/users.json')
-	.success(function(data) {
-		console.log(data);
 
-		for (var key in data) {
-			var obj = data[key];
-			if (obj.name === $scope.name) {
-				console.log('works');
-				$scope.user = obj;
-			}
-			else {
-				console.log('no');
-			}
-		}
-	});
+myapp.controller('MainCtrl', function ($scope, $firebase, $routeParams, $http) {
+    $scope.searchOnClick = function () {
+        var ref = new Firebase('https://devflow.firebaseio.com/users');
+        var sync = $firebase(ref);
+        $scope.user = {};
+        $http.get('https://devflow.firebaseio.com/users.json')
+        .success(function(data) {
+
+            for (var key in data) {
+                var obj = data[key];
+                if (obj.name === $scope.query) {
+                    $scope.name="works";
+                    console.log($scope.name);
+                    $scope.user = obj;
+                    break;
+                }
+                else {
+                    $scope.name="no";
+                    console.log($scope.name);
+                }
+            }
+        });
+    };
   });
